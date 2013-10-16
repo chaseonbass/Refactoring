252c8acee4b9712add9063dbb5e7a366f8585f6a
Chase <cwest6160@yahoo.com>
2013-10-16 15:45:50 -0600

    added constructor to WalkwayCell()

--- clueGame/WalkwayCell.java
+++ clueGame/WalkwayCell.java
@@ -1,6 +1,11 @@
 package clueGame;
 
 public class WalkwayCell extends BoardCell{
+	public WalkwayCell(int row, int column, String cell) {
+		super(row, column, cell);
+		// TODO Auto-generated constructor stub
+	}
+
 	public boolean isWalkway(){
 		return true;
 	}
