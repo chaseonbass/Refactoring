af236a96db42a0fd34a96423ee43feba25559b67
Evan Breikss <ebreikss@mines.edu>
2013-10-22 16:00:59 -0600

    Passing tests for Selecting Target Locations

--- clueGame/ComputerPlayer.java
+++ clueGame/ComputerPlayer.java
@@ -1,6 +1,8 @@
 package clueGame;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class ComputerPlayer extends Player{
@@ -23,6 +25,29 @@ public class ComputerPlayer extends Player{
 	public BoardCell pickLocation(Set<BoardCell> t){
 		// this uses 'lastRoomVisited'
 		// Temporary return type WalkwayCell. Can be either a RoomCell or WalkwayCell
+		ArrayList<BoardCell> tempUnvisitedDoor = new ArrayList<BoardCell>();
+		for(BoardCell b : t) {
+			if(b.isDoorway() && b.getInitial() != lastRoomVisited)
+				tempUnvisitedDoor.add(b);
+		}
+		
+		Random rand = new Random();
+		
+		if(!tempUnvisitedDoor.isEmpty()){
+			int randNum = rand.nextInt(t.size()) % tempUnvisitedDoor.size();
+			return tempUnvisitedDoor.get(randNum);
+		} else {
+			int randNum = rand.nextInt(t.size()) % t.size();
+			int i = 0;
+			for (BoardCell b : t) {
+				if (i == randNum)
+					return b;
+				else
+					i++;
+			}
+		}
+		
+		// As a last resort, if it's failing, this is the 'failure' condition
 		return new WalkwayCell(0,0,"X");
 	}
 	
